99e8a736b146e3865e086d1b5bb0ef6a13379456
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-06-06 08:55:31 +0530

    SideBar created

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ import streamlit as st
 
 st.title("ChatBot")
 
+# Chat Input
 prompt = st.chat_input("Ask a question")
 
 if prompt:
     st.write(f"You asked: {prompt}")
+
+# SideBar
+st.set_page_config(
+    page_title="AI Assistant",
+    page_icon="🤖",
+    layout="wide"
+)
+
+with st.sidebar:
+    st.title("🤖 AI Assistant")
+    st.markdown("---")
+    st.write("Modes")
+    st.success("LLM")
+    st.info("Web Search")
+    st.warning("RAG")
